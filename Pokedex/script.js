4d86b1b2a133f319ba9b08f94fcd2fd75e21d7ad
Vasudev Kishan <vasudevkishan@gmail.com>
2025-01-19 16:33:47 +0530

    pokedex - removed unnecessary commented styles and added lightend colors for shiny

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -3,20 +3,37 @@ const poke_container = document.getElementById("poke-container");
 const pokemon_count = 150;
 
 const colors = {
-    fire: "#FDDFDF",
+    fire: "#fbd4d4",
     grass: "#98d7a5",
-    electric: "#f6f9be",
-    water: "#DEF3FD",
+    electric: "#fbffb7",
+    water: "#c6e2ef",
     ground: "#dfba95",
     rock: "#d5d5d4",
     fairy: "#fceaff",
     poison: "#cb92ec",
-    bug: "#DEFDE0",
-    dragon: "#97b3e6",
-    psychic: "#eaeda1",
+    bug: "#c1ffc5",
+    dragon: "#fe7373",
+    psychic: "#5ae9ff",
     flying: "#F5F5F5",
     fighting: "#E6E0D4",
     normal: "#c3c3c3",
+};
+
+const lightendColors = {
+    fire: "#f4dcdc",
+    grass: "#a9eeb6",
+    electric: "#fdffd2",
+    water: "#d8f2fe",
+    ground: "#ffd5ab",
+    rock: "#b1b1b0",
+    fairy: "#dac7de",
+    poison: "#e3b3fe",
+    bug: "#DEFDE0",
+    dragon: "#eb8181",
+    psychic: "#b7f5ff",
+    flying: "#d1d0f6",
+    fighting: "#fff9ed",
+    normal: "#d5d5d5",
 };
 
 const main_types = Object.keys(colors);
@@ -43,11 +60,12 @@ const createPokemonCard = (pokemon) => {
     const poke_types = pokemon.types.map((type) => type.type.name);
     const type = main_types.find((type) => poke_types.indexOf(type) > -1);
     const color = colors[type];
+    const shinyColor = lightendColors[type];
 
-    pokemonEl.style.backgroundColor = color;
+    // pokemonEl.style.backgroundColor = color;
 
     const pokemonInnerHTML = `
-    <div class="face front">
+    <div class="face front" style="background-color: ${color};">
         <div class="img-container">
                     <img
                         src=${pokemon.sprites.other["official-artwork"].front_default}
@@ -60,7 +78,7 @@ const createPokemonCard = (pokemon) => {
                     <small class="type">Type: <span>${type}</span></small>
                 </div>
     </div>
-    <div class="face back">
+    <div class="face back" style="background-color: ${shinyColor};">
         <div class="img-container">
                     <img
                         src=${pokemon.sprites.other["official-artwork"].front_shiny}
